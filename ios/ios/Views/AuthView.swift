069053df544233212c8ee44228147b53be506f9a
masakichi41 <masakichi410213@gmail.com>
2025-10-18 18:44:16 +0900

    feat: Login / Logout

---
+++ ios/ios/Views/AuthView.swift
@@ -0,0 +1,52 @@
+//
+//  CardView.swift
+//  ios
+//
+
+import SwiftUI
+import Auth0
+
+struct AuthView: View {
+    @State var user: User?
+
+    var body: some View {
+        if let user = self.user {
+            VStack {
+                ContentView()
+                Button("Logout", action: self.logout)
+            }
+        } else {
+            Button("Login", action: self.login)
+        }
+    }
+}
+
+extension AuthView {
+    func login() {
+        Auth0
+            .webAuth()
+            .useHTTPS() // Use a Universal Link callback URL on iOS 17.4+ / macOS 14.4+
+            .start { result in
+                switch result {
+                case .success(let credentials):
+                    self.user = User(from: credentials.idToken)
+                case .failure(let error):
+                    print("Failed with: \(error)")
+                }
+            }
+    }
+
+    func logout() {
+        Auth0
+            .webAuth()
+            .useHTTPS() // Use a Universal Link logout URL on iOS 17.4+ / macOS 14.4+
+            .clearSession { result in
+                switch result {
+                case .success:
+                    self.user = nil
+                case .failure(let error):
+                    print("Failed with: \(error)")
+                }
+            }
+    }
+}
---
+++ ios/ios/Views/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  ios
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    let user: User
+
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: user.picture))
+            Text("Email: \(user.email)")
+        }
+    }
+}
