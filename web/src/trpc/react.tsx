af8d17825c92fea31f0b13f9b2c2c1b70711addf
masakichi41 <masakichi410213@gmail.com>
2025-10-18 21:00:42 +0900

    fix: cookie settings

--- web/src/trpc/react.tsx
+++ web/src/trpc/react.tsx
@@ -59,6 +59,12 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
             headers.set("x-trpc-source", "nextjs-react");
             return headers;
           },
+          fetch(url, options) {
+            return fetch(url, {
+              ...options,
+              credentials: "include",
+            });
+          },
         }),
       ],
     }),
